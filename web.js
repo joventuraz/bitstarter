8fb2028f3509bf35cf89389edab0053180b879af
Jose Ventura <jo.venturaz@gmail.com>
2013-12-30 00:09:40 +0000

    Fix buffer

--- web.js
+++ web.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var app = express();
+var fs = require('fs');
 app.use(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+    var fileContent = fs.readFileSync('index.html');
+    response.send(fileContent.toString("utf-8"));
 });
 
 var port = process.env.PORT || 5000;
